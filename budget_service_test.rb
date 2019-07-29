098f5d76c00018434aaea316f2888e05f72e2be5
jimytc <solofat@gmail.com>
2019-07-30 00:38:52 +0800

    Make when statement more readable

--- budget_service_test.rb
+++ budget_service_test.rb
@@ -19,7 +19,7 @@ class BudgetServiceTest < Minitest::Test
 
   def test_no_budget
     given_no_budget do
-      when_period_is date_of('20200401'), date_of('20200401') do
+      when_period_starts from('20200401'), to('20200401') do
         budget_should_be 0.0
       end
     end
@@ -27,7 +27,7 @@ class BudgetServiceTest < Minitest::Test
 
   def test_period_inside_budget_month
     given_budgets Budget.new('202004', 30) do
-      when_period_is date_of('20200401'), date_of('20200401') do
+      when_period_starts from('20200401'), to('20200401') do
         budget_should_be 1.0
       end
     end
@@ -35,7 +35,7 @@ class BudgetServiceTest < Minitest::Test
 
   def test_period_no_overlap_before_budget_first_day
     given_budgets Budget.new('202004', 30) do
-      when_period_is date_of('20200331'), date_of('20200331') do
+      when_period_starts from('20200331'), to('20200331') do
         budget_should_be 0.0
       end
     end
@@ -43,7 +43,7 @@ class BudgetServiceTest < Minitest::Test
 
   def test_period_no_overlap_after_budget_last_day
     given_budgets Budget.new('202004', 30) do
-      when_period_is date_of('20200501'), date_of('20200501') do
+      when_period_starts from('20200501'), to('20200501') do
         budget_should_be 0.0
       end
     end
@@ -51,7 +51,7 @@ class BudgetServiceTest < Minitest::Test
 
   def test_period_overlap_budget_last_day
     given_budgets Budget.new('202004', 30) do
-      when_period_is date_of('20200430'), date_of('20200501') do
+      when_period_starts from('20200430'), to('20200501') do
         budget_should_be 1.0
       end
     end
@@ -59,7 +59,7 @@ class BudgetServiceTest < Minitest::Test
 
   def test_period_cross_budget_month
     given_budgets Budget.new('202004', 30) do
-      when_period_is date_of('20200331'), date_of('20200501') do
+      when_period_starts from('20200331'), to('20200501') do
         budget_should_be 30.0
       end
     end
@@ -67,7 +67,7 @@ class BudgetServiceTest < Minitest::Test
 
   def test_invalid_period
     given_budgets Budget.new('202004', 30) do
-      when_period_is date_of('20200430'), date_of('20200401') do
+      when_period_starts from('20200430'), to('20200401') do
         budget_should_be 0.0
       end
     end
@@ -75,7 +75,7 @@ class BudgetServiceTest < Minitest::Test
 
   def test_daily_amount_10
     given_budgets Budget.new('202004', 300) do
-      when_period_is date_of('20200401'), date_of('20200402') do
+      when_period_starts from('20200401'), to('20200402') do
         budget_should_be 20.0
       end
     end
@@ -83,7 +83,7 @@ class BudgetServiceTest < Minitest::Test
 
   def test_multiple_budgets
     given_budgets Budget.new('202003', 310), Budget.new('202004', 30), Budget.new('202005', 3100) do
-      when_period_is date_of('20200330'), date_of('20200501') do
+      when_period_starts from('20200330'), to('20200501') do
         budget_should_be 150.0
       end
     end
@@ -100,7 +100,7 @@ class BudgetServiceTest < Minitest::Test
     yield if block_given?
   end
 
-  def when_period_is(start_date, end_date)
+  def when_period_starts(start_date, end_date)
     block = yield if block_given?
     block.call(@service.query(start_date, end_date)) if block.respond_to?(:call)
   end
@@ -109,7 +109,11 @@ class BudgetServiceTest < Minitest::Test
     ->(actual) { assert_in_delta expected, actual }
   end
 
-  def date_of(date_str)
+  def from(date_str)
+    Date.strptime(date_str, '%Y%m%d')
+  end
+
+  def to(date_str)
     Date.strptime(date_str, '%Y%m%d')
   end
 end
